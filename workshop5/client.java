0fc5a9ecd9d7f7b21c42f9d3d803ce294206e813
COMP3100 JinjiaBai <jinjia.bai@students.mq.edu.au>
2023-03-23 00:20:26 +1100

    try to use GETScmd

---
+++ workshop5/client.java
@@ -0,0 +1,45 @@
+import java.net.*;  
+import java.io.*;  
+class MyClient{  
+public static void main(String args[])throws Exception{  
+Socket s=new Socket("localhost",50000);  //port
+//DataInputStream din=new DataInputStream(s.getInputStream());  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+//Handshaking
+String Rep=""; 
+//send HELO
+dout.write(("HELO\n").getBytes());
+dout.flush(); 
+//get OK about accessing server successfully
+Rep=in.readLine();
+if(!Rep.equals("OK")) System.out.println("ERROR 250");
+else System.out.println("Server says: "+Rep);
+//send username
+String username = System.getProperty("user.name");
+dout.write(("AUTH "+username+"\n").getBytes());
+dout.flush(); 
+//get OK about AUTH from server
+Rep=in.readLine();
+if(Rep.equals("OK")) System.out.println("Welcome "+username);
+//send REDY
+dout.write(("REDY\n").getBytes());
+dout.flush();
+Rep=in.readLine();
+System.out.println(Rep);
+
+String str="",str2="";  
+while(!str.equals("QUIT")){  
+str=br.readLine();  
+dout.write((str+"\n").getBytes());  
+dout.flush();  
+str2=in.readLine();  
+System.out.println("Server says: "+str2);  
+str = str2;
+}  
+  
+dout.close();  
+s.close();  
+}}  

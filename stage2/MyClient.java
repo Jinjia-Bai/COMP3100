20f66cb02192248d17f50ac8b6ad918db56dae11
COMP3100 JinjiaBai <jinjia.bai@students.mq.edu.au>
2023-05-19 18:13:24 +1000

    start_work

--- stage2/MyClient.java
+++ stage2/MyClient.java
@@ -2,80 +2,12 @@ import java.net.*;
 import java.io.*;  
 class MyClient{  
 	public static void main(String args[])throws Exception{  
-		Socket s=new Socket("localhost",50000);  //port  
+		Socket s = new Socket("localhost",50000);  //port  
 		DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
 		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
 		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		
 		//Handshaking
-		Handshake();
-		
-		//select which algorithm will be used
-		if (args
-		
-		//job 1-n
-		boolean isGETS = true;
-		int jobID = 0, max = 0, nRecs = 0, n = 0, cores = 0;
-		String type = "";
-		String[] Array = null;
-		while(!str.equals("NONE")){
-			//send REDY
-			dout.write(("REDY\n").getBytes());
-			dout.flush();
-			//receive JOBN,JCPL or NONE
-			str2 = in.readLine();
-			//System.out.println("Server says: "+str2);
-			if(isGETS){//do it only once in the while loop
-				//send GETS All
-				dout.write(("GETS All\n").getBytes());
-				dout.flush();
-				//Receive DATA nRecs recSize
-				str = in.readLine();
-				//System.out.println("Server says: "+str);
-				Array = str.split(" ");
-				nRecs = Integer.parseInt(Array[1]);
-				//Send OK
-				OK();
-				//receive records and identify the largest server type
-				for(int i = 0;i<nRecs;i++){
-					String[] Array2 = null;
-					str = in.readLine();
-					//System.out.println("Server says: "+str);
-					Array2 = str.split(" ");
-					cores = Integer.parseInt(Array2[4]);
-					if(cores > max){
-						type = Array2[0];
-						max = cores;
-					}
-					if(type.equals(Array2[0])) n = Integer.parseInt(Array2[1]);
-				}
-				n++;
-				//Send OK
-				OK();
-				//receive .
-				str = in.readLine();
-				//System.out.println("Server says: "+str);
-				isGETS = false;
-			}
-
-			//schedule a job
-			if(str2.contains("JOBN")){
-				dout.write(("SCHD "+jobID+" "+type+" "+jobID%n+"\n").getBytes());
-				dout.flush();
-				jobID++;
-				//receive OK
-				str = in.readLine();
-				//System.out.println("Server says: "+str);
-			}
-			else str = str2;
-		}
-		
-		//send QUIT
-		QUIT();
-	}
-
-
-	static void Handshake(){
 		String str = "",str2 = "";
 		//send HELO
 		dout.write(("HELO\n").getBytes());
@@ -91,14 +23,72 @@ class MyClient{
 		//receive OK
 		str = in.readLine();
 		//System.out.println("Welcome "+username);
-	}
-	
-	static void OK(){
-		dout.write(("OK\n").getBytes());
-		dout.flush();
-	}
-	
-	static void QUIT(){
+		
+		//job 1-n
+		int jobID = 0, max = 0, nRecs = 0, n = 0, core = 0, memory = 0, disk = 0, serverID = 0, first_serverID = 0, wjobs = 0, rjobs = 0;
+		String type = "", first_type = "";
+		String[] array = null;
+		while(!str.equals("NONE")){
+			boolean isFirst = true;
+			//send REDY
+			dout.write(("REDY\n").getBytes());
+			//receive JOBN,JCPL or NONE
+			str2 = in.readLine();
+			//System.out.println("Server says: "+str2);
+			if(str2.contains("JOBN")){
+				array = str2.split(" ");
+				jobID = Integer.parseInt(array[2]);
+				core = Integer.parseInt(array[4]);
+				memory = Integer.parseInt(array[5]);
+				disk = Integer.parseInt(array[6]);
+				//send GETS All
+				dout.write(("GETS Capable "+core+" "+memory+" "+disk+"\n").getBytes());
+				dout.flush();
+				//Receive DATA nRecs recSize
+				str = in.readLine();
+				//System.out.println("Server says: "+str);
+				String[] array3=null;
+				array3 = str.split(" ");
+				nRecs = Integer.parseInt(array3[1]);
+				dout.write(("OK\n").getBytes());
+				dout.flush();//Send OK
+				//receive records and identify the First Capable server
+				for(int i = 0;i<nRecs;i++){
+					String[] array2=null;
+					str=in.readLine();
+					//System.out.println("Server says: "+str);
+					array2 = str.split(" ");
+					if(isFirst){
+						first_type = array2[0];
+						first_serverID = Integer.parseInt(array2[1]);
+						isFirst = false;
+					}
+					wjobs = Integer.parseInt(array2[7]);
+					rjobs = Integer.parseInt(array2[8]);
+					if(wjobs != 1 || rjobs != 1){
+						type = array2[0];
+						serverID = Integer.parseInt(array2[1]);
+					}
+					else{
+						type = first_type;
+						serverID = first_serverID;
+					}
+				}
+				dout.write(("OK\n").getBytes());
+				dout.flush();//Send OK
+				//receive .
+				str = in.readLine();
+				//System.out.println("Server says: "+str);
+				//schedule a job
+				dout.write(("SCHD "+jobID+" "+type+" "+serverID+"\n").getBytes());
+				dout.flush();
+				//receive OK
+				str = in.readLine();
+				//System.out.println("Server says: "+str);
+			}	
+			else str = str2;
+		}
+		
 		//send QUIT
 		dout.write(("QUIT\n").getBytes());
 		dout.flush();
@@ -107,4 +97,10 @@ class MyClient{
 		dout.close(); 
 		s.close();
 	}
+	
+	/*static void OK(){
+		dout.write(("OK\n").getBytes());
+		dout.flush();
+	}*/
+	
 }

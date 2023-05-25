51978e0fedc0648e208606e22f7f636512fb8fe9
COMP3100 JinjiaBai <jinjia.bai@students.mq.edu.au>
2023-05-25 17:03:23 +1000

    fixed_style

--- stage2/test.java
+++ stage2/test.java
@@ -1,6 +1,6 @@
 import java.net.*;  
 import java.io.*;  
-class MyClient{
+class test{
 	public static void main(String args[])throws Exception{  
 		Socket s = new Socket("localhost",50000);  //port  
 		DataOutputStream dout = new DataOutputStream(s.getOutputStream());  
@@ -24,16 +24,16 @@ class MyClient{
 		str = in.readLine();
 		
 		//job 1-n
-		int jobID = 0, max = 0, nRecs = 0, core = 0, memory = 0, disk = 0,cores = 0, memorys = 0, disks = 0, serverID = 0, wjobs = 0, first_serverID = 0;
-		String type = "", first_type = "";
+		int jobID = 0, max = 0, nRecs = 0, core = 0, memory = 0, disk = 0,cores = 0, memorys = 0, disks = 0, serverID = 0, wjobs = 0;
+		String type = "";
 		String[] array = null;
 		while(!str.equals("NONE")){
-			boolean isFirst = true, isGet = false;
+			boolean isGet = false;
 			//send REDY
 			dout.write(("REDY\n").getBytes());
 			//receive JOBN, JOBP, JCPL, CHKQ or NONE
 			str2 = in.readLine();
-			if(str2.contains("JOBN")){
+			if(str2.contains("JOBN") || str2.contains("JOBP")){
 				array = str2.split(" ");
 				jobID = Integer.parseInt(array[2]);
 				core = Integer.parseInt(array[4]);
@@ -44,20 +44,15 @@ class MyClient{
 				dout.flush();
 				//Receive DATA nRecs recSize
 				str = in.readLine();
-				String[] array3 = null;
+				String[] array3=null;
 				array3 = str.split(" ");
 				nRecs = Integer.parseInt(array3[1]);
 				sendOK(dout);
 				//receive records
 				for(int i = 0;i<nRecs;i++){
-					String[] array2 = null;
-					str = in.readLine();
+					String[] array2=null;
+					str=in.readLine();
 					array2 = str.split(" ");
-					if(isFirst){
-						first_type = array2[0];
-						first_serverID = Integer.parseInt(array2[1]);
-						isFirst = false;
-					}
 					cores = Integer.parseInt(array2[4]);
 					memorys = Integer.parseInt(array2[5]);
 					disks = Integer.parseInt(array2[6]);
@@ -75,11 +70,21 @@ class MyClient{
 				
 				//no s* found
 				if(isGet == false){
-					type = first_type;
-					serverID = first_serverID;
+					dout.write(("ENQJ GQ" + "\n").getBytes());
+					dout.flush();
+					//receive OK
+					str = in.readLine();
 				}
 				//schedule a job
+				else{
 				dout.write(("SCHD " + jobID + " " + type + " " + serverID + "\n").getBytes());
+				dout.flush();
+				//receive OK
+				str = in.readLine();
+				}
+			}
+			else if(str2.contains("CHKQ")){
+				dout.write(("DEQJ GQ 0" + "\n").getBytes());
 				dout.flush();
 				//receive OK
 				str = in.readLine();
